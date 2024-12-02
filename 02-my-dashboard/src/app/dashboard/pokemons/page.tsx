2c75c71c375472c25d99e78dd465136b4db072d3
Miguel García Reimers <mgarciareimers@gmail.com>
2024-12-02 23:36:11 +0100

    Pokemons page developed + Pokemon detail page developed.

---
+++ 02-my-dashboard/src/app/dashboard/pokemons/page.tsx
@@ -0,0 +1,25 @@
+import { PokemonGrid, PokemonsResponse, SimplePokemon } from '@/pokemons';
+
+const getPokemons = async (limit: number = 20, offset: number = 0): Promise<SimplePokemon []> => {
+    const data: PokemonsResponse = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${ limit }&offset=${ offset }`)
+        .then((response: Response) => response.json());
+
+    const pokemons = data.results.map((pokemon: any) => ({
+        id: pokemon.url.split('/').at(-2),
+        name: pokemon.name
+    }));
+
+    return pokemons;
+}
+
+export default async function PokemonsPage() {
+    const pokemons: SimplePokemon [] = await getPokemons(151);
+
+    return (
+        <div className='flex flex-col'>
+            <span className='text-5xl my-2'>Pokemons List <small>(static)</small></span>
+            
+            <PokemonGrid pokemons={ pokemons } />
+        </div>
+    );
+}
---
+++ 02-my-dashboard/src/pokemons/index.ts
@@ -0,0 +1,5 @@
+export { PokemonGrid } from './components/PokemonGrid';
+
+export type { Pokemon } from './interfaces/pokemon';
+export type { PokemonsResponse } from './interfaces/pokemons-response';
+export type { SimplePokemon } from './interfaces/simple-pokemon';
